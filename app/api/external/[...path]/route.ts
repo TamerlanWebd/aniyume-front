e84cf504038f48e60711a975b01d4cba0a76e396
TamerlanWebd <zanshugurov07@gmail.com>
2026-01-03 02:07:19 +0500

    fix: update parameter type in handleProxy function and clean up package-lock.json

--- app/api/external/[...path]/route.ts
+++ app/api/external/[...path]/route.ts
@@ -4,15 +4,15 @@ const BASE_URL = "http://164.90.185.95/api/v1";
 
 export async function handleProxy(
   req: NextRequest,
-  { params }: { params: { path: string[] } }
+  { params }: { params: Promise<{ path: string[] }> }  
 ) {
   try {
-    const { path } = await params;
+    const { path } = await params;  
     const pathStr = path.join("/");
     const searchParams = req.nextUrl.search;
 
     const privateKeywords = [
-       "user",
+      "user",
       "status",
       "profile",
       "favorites",
