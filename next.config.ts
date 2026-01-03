fd19b285cfb873ae23c8662f3a92aa0dbabf5007
TamerlanWebd <zanshugurov07@gmail.com>
2026-01-03 11:10:29 +0500

    fix: merge configs and move tailwind to dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   images: {
     domains: [
       'picsum.photos',
@@ -12,10 +13,34 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: '**',
       },
+      {
+        protocol: 'http',
+        hostname: '164.90.185.95',
+      },
     ],
     unoptimized: true,
   },
-
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-eval' 'unsafe-inline';
+              style-src 'self' 'unsafe-inline';
+              img-src 'self' data: https: http:;
+              font-src 'self' https: data:;
+              frame-src 'self' https: http:;
+              connect-src 'self' http://164.90.185.95 https://covers-determination-compliance-outlets.trycloudflare.com;
+            `.replace(/\s{2,}/g, ' ').trim(),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
